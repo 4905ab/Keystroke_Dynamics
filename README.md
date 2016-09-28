fd8620cdbfe2aa8859e250239a53fb3c3b2452db
4905ab <atb1amt@bolton.ac.uk>
2016-09-28 17:32:03 +0100

    added chris code
    added the code from the distation and changed the settings so it is
    named correct

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# keystroke_dynamics
+# Keystroke_Dynamics_Accidentally_Kangaroo
 To produce a Phyon script program that will take a user's input from a PHP form and calculate some form finger print for the server to keep long side the user name and password so when they login it can used to aid the login possess  
---
+++ chriscode.py
@@ -0,0 +1,15 @@
+import phythoncom, pyHook, time
+from datetime import datetime
+
+def OnKeyboardEvent(event):
+    start = time.time()
+    print 'Ascii', event.Ascii, chr(event.Ascii)
+    end = (time.time() - start)
+    print end
+
+    return True
+
+hooks_manager = pyHook.HookManager()
+hooks_manager.Keydown = OnKeyboardEvent
+hooks_manager.HookManager()
+phythoncom.PumpMessages()
