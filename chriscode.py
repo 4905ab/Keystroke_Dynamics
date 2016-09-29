d3f9c9e05276eae0401437ee809510ba5b3f453d
4905ab <atb1amt@bolton.ac.uk>
2016-09-29 16:56:38 +0100

    debugged

--- chriscode.py
+++ chriscode.py
@@ -1,15 +1,17 @@
-import phythoncom, pyHook, time
-from datetime import datetime
+import pyHook
+from datetime import time
+import pythoncom
+
 
 def OnKeyboardEvent(event):
     start = time.time()
-    print 'Ascii:', event.Ascii, chr(event.Ascii)
+    print('Ascii:', event.Ascii, chr(event.Ascii))
     end = (time.time() - start)
-    print end
+    print(end)
 
     return True
 
 hooks_manager = pyHook.HookManager()
 hooks_manager.Keydown = OnKeyboardEvent
 hooks_manager.HookManager()
-phythoncom.PumpMessages()
+pythoncom.PumpMessages()
